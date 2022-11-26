523aa083f361b62513ac7de392df1b7880fbb96d
MUPPALA-SAI-SHASHANK <108987073+MUPPALA-SAI-SHASHANK@users.noreply.github.com>
2022-11-26 17:33:36 +0530

    Add files via upload

---
+++ TASK1-JAVASCRIPT_QUIZ_GAME/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+     "Hyper Text Preprocessor",
+     "Hyper Text Markup Language",
+     "Hyper Text Multiple Language",
+     "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheets",
+    options: [
+      "Cascade Style Sheets",
+      "Color and Style Sheets",
+      "Cascading Style Sheets",
+      "None of the Above"
+    ]
+  },
+  {
+    numb: 3,
+    question: "The HTML attribute used to define the inline styles is ?",
+    answer: "style",
+    options: [
+      "styles",
+      "style",
+      "class",
+      "None of the Above"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which of the following property is used as the shorthand property for the padding properties?",
+    answer: "padding",
+    options: [
+      "padding",
+      "padding-left",
+      "padding-right",
+      "All of the Above"
+    ]
+  },
+  {
+    numb: 5,
+    question: "The CSS property used to specify whether the text is written in the horizontal or vertical direction?",
+    answer: "writing-mode",
+    options: [
+      "text-intent",
+      "word-break",
+      "text-break",
+      "writing-mode"
+    ]
+  },
+];
